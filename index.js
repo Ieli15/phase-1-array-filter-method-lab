e6b392f72b3227f316eea7bc8ef1ff891dc1179a
Elias Bosire Ayunga <bosire.elias@gmail.com>
2024-12-06 07:57:19 +0300

    First commit

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+// findMatching
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  // fuzzyMatch
+  function fuzzyMatch(drivers, string) {
+    return drivers.filter(driver => driver.startsWith(string));
+  }
+
+  // matchName
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+   
